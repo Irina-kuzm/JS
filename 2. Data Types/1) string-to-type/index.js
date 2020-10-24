0341cbc5bd47793fca48bd75031c8a35bb1450fd
user <kuzmenko.exceedteam@gmail.com>
2020-10-24 18:48:39 +0200

    String-to-type

--- 2. Data Types/1) string-to-type/index.js
+++ 2. Data Types/1) string-to-type/index.js
@@ -1,5 +1,33 @@
 function stringToType(str) {
 
+	let k = isNaN(str);
+
+	if (k === false) {
+
+		if (str == "")  {
+			return str;
+		}
+		else {
+			return str = +str;
+		}
+	}
+
+	switch (str) {
+		case 'false':
+			str = false;
+			break;
+		case 'true':
+			str = true;
+			break;
+		case 'null':
+			str = null;
+			break;
+		case 'undefined':
+			str = undefined;
+			break;
+	}
+
+	return str;
 };
 
 window.stringToType = stringToType;
