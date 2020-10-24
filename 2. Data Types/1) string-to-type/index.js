8a2f15b78b1c81be8a7400804edc7587e6a5f9d1
user <kuzmenko.exceedteam@gmail.com>
2020-10-24 18:17:07 +0200

    string-to-type

--- 2. Data Types/1) string-to-type/index.js
+++ 2. Data Types/1) string-to-type/index.js
@@ -1,6 +1,34 @@
 function stringToType(str) {
-  // Напиши свой код здесь
-};
+
+	let k = isNaN(str);
+
+	if (k==false) {
+		if (str == '') {
+			return str;
+		}
+		else {
+			return str = Number(str);
+		}
+		
+	}
+	
+	switch (str) {
+		case 'true':
+			str = true;
+			break;
+		case 'false':
+			str = false;
+			break;
+		case 'null':
+			str = null;
+			break;
+		case 'undefined':
+			str = undefined;
+			break;
+	}
+	return str;
+
+	};
 
 window.stringToType = stringToType;
 
