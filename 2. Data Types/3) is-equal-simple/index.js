260443d031970c10f3c193e89945ab0cfe0190ba
user <kuzmenko.exceedteam@gmail.com>
2020-10-28 09:15:11 +0300

    is-equal-simple

--- 2. Data Types/3) is-equal-simple/index.js
+++ 2. Data Types/3) is-equal-simple/index.js
@@ -1,28 +1,60 @@
 function isEqual(a, b) {
 
 
-	for (let value in a)  {
-		if (a.value === undefined)  {
-			a.value = 'undefined';
-		}
-	}
-	for (let value in b)  {
-		if (b.value === undefined)  {
-			b.value = 'undefined';
-		}
-	}
+var p;
 
+    if (isNaN(a) && isNaN(b) && typeof a === 'number' && typeof b === 'number') {
+         return true;
+    }
+    if (a === b) {
+        return true;
+    }
+    if ((typeof a === 'function' && typeof b === 'function') ||
+       (a instanceof Date && b instanceof Date) ||
+       (a instanceof RegExp && b instanceof RegExp) ||
+       (a instanceof String && b instanceof String) ||
+       (a instanceof Number && b instanceof Number)) {
+        return a.toString() === b.toString();
+    }
 
-	return JSON.stringify(a) == JSON.stringify(b); 
+    // At last checking prototypes as good as we can
+    if (!(a instanceof Object && b instanceof Object)) {
+        return false;
+    }
 
-	
+    if (a.isPrototypeOf(b) || b.isPrototypeOf(a)) {
+        return false;
+    }
 
+    if (a.constructor !== b.constructor) {
+        return false;
+    }
 
+    if (a.prototype !== b.prototype) {
+        return false;
+    }
 
-	
+    for (p in b) {
+        if (b.hasOwnProperty(p) !== a.hasOwnProperty(p)) {
+            return false;
+        }
+        else if (typeof b[p] !== typeof a[p]) {
+            return false;
+        }
+    }
 
-	
+    for (p in a) {
+        if (b.hasOwnProperty(p) !== a.hasOwnProperty(p)) {
+            return false;
+        }
+        else if (typeof b[p] !== typeof a[p]) {
+            return false;
+        }
 
+       
+    }
+
+    return true;
 
 
 }
